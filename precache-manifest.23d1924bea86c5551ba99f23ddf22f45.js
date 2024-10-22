25cf490d19b5b91e34deba402cbd62fa8ff410bc
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2024-10-21 19:54:48 -0500

    Updates

---
+++ precache-manifest.23d1924bea86c5551ba99f23ddf22f45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfae7b167d6aeddff5e8",
+    "url": "/me/static/css/main.eb285aec.chunk.css"
+  },
+  {
+    "revision": "cfae7b167d6aeddff5e8",
+    "url": "/me/static/js/main.cfae7b16.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "00fa614b176605f62ba3573a11523cd3",
+    "url": "/me/index.html"
+  }
+];
